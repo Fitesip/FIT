2001f0a22030a02610c94ac00bef030d65d6dbb7
Fitesip <50716281+Fitesip@users.noreply.github.com>
2024-04-26 00:57:26 +0300

    Update-1

---
+++ fonts/strangferfixcs/readme strangerfixc.txt
@@ -0,0 +1,7 @@
+strangerfixc.ttf is a free for personal use, is an amateur design =)
+
+my site: http://truefonts.blogspot.com
+
+direct link to the font http://truefonts.blogspot.com/2012/10/newfont.html
+
+kaiserzharkhan
---
+++ scripts/animation.js
@@ -0,0 +1,28 @@
+let card_arr = [
+    card1 = document.getElementById("card1"),
+    card2 = document.getElementById("card2"),
+    card3 = document.getElementById("card3"),
+    card4 = document.getElementById("card4"),
+    card5 = document.getElementById("card5"),
+    card6 = document.getElementById("card6"),
+];
+
+let img_arr = [
+    img1 = document.getElementById("1"),
+    img2 = document.getElementById("2"),
+    img3 = document.getElementById("3"),
+    img4 = document.getElementById("4"),
+    img5 = document.getElementById("5"),
+    img6 = document.getElementById("6"),
+];
+
+for (let i = 0; i<card_arr.length; i++){
+    card_arr[i].onmouseleave = function (){
+        img_arr[i].style.marginTop = '-325' + 'px';
+        card_arr[i].style.height = '148' + 'px';
+    };
+    card_arr[i].onmouseenter = function () {
+        img_arr[i].style.marginTop = '0' + 'px';
+        card_arr[i].style.height = '475' + 'px';
+    }
+}
